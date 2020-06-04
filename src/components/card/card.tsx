463ac09380fc96e55123babcd74c3fd51207e7bc
Mike Wright <miwright@grubhub.com>
2020-06-04 08:24:41 -0400

    Adds card stylings to not look like black squares

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -3,12 +3,16 @@ import styles from './card.module.css';
 import cn from 'classnames';
 import { Card as CardType } from '../../../types/card';
 
+const CARD_HEIGHT = 123;
+const CARD_WIDTH = 79;
+
 export function Card({ card }: { card: CardType }): JSX.Element {
     return (
         <div
+            style={{
+                backgroundPosition: `-${card.value * CARD_WIDTH}px -${card.suit * CARD_HEIGHT}px`,
+            }}
             className={cn('flex-center-center', styles.card, { [styles.cardHidden]: card.hidden })}
-        >
-            {!card.hidden && card.value}
-        </div>
+        ></div>
     );
 }
